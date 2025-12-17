82e2a2f2013d9ce672e3f8250f8d09a406701617
dfppr <107608037+dfppr@users.noreply.github.com>
2025-12-17 22:43:03 +0700

    Delete venv directory

--- venv/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_puper_duper_very_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data', 'blog.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
